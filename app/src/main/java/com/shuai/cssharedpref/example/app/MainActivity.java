ef38615890af2f8ea33fe5b2ad74ac8b544dafc0
常帅 <changshuai@renrenche.com>
2019-11-06 14:15:25 +0800

    删除无用注释

--- app/src/main/java/com/shuai/cssharedpref/example/app/MainActivity.java
+++ app/src/main/java/com/shuai/cssharedpref/example/app/MainActivity.java
@@ -9,24 +9,45 @@ import com.shuai.sharedpref.SharedPref;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String KEY = "mykey";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 存储数据
         findViewById(R.id.btn_save_data).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                SharedPref.setString("KEY", "我是数据");
+                SharedPref.setString(KEY, "我是数据");
             }
         });
+
+        // 获取数据
         findViewById(R.id.btn_get_data).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String key = SharedPref.getString("KEY", "-");
+                String key = SharedPref.getString(KEY, "-");
 
                 Toast.makeText(MainActivity.this, key, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 删除数据
+        findViewById(R.id.btn_remove_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPref.removeKey(KEY);
+            }
+        });
+
+        // 判断数据是否存在
+        findViewById(R.id.btn_judge_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, SharedPref.contains(KEY)+"", Toast.LENGTH_SHORT).show();
             }
         });
     }
